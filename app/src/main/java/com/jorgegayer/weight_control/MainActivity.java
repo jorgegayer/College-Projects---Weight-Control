f1295d92448f44ac214a3be4e62e02635fe41546
Arianne <a.hoffschneidergu002@mybvc.ca>
2022-03-11 10:23:05 -0700

    Fields validation, date picker and save state
    Everything was done during a meeting

--- app/src/main/java/com/jorgegayer/weight_control/MainActivity.java
+++ app/src/main/java/com/jorgegayer/weight_control/MainActivity.java
@@ -1,8 +1,12 @@
 package com.jorgegayer.weight_control;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void callProfile(View view) {
+        startActivity(new Intent(MainActivity.this, ProfilePage.class));
+
+    }
+
+    public void showDate(View view) {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "DatePicker");
+    }
+
+    public void processDatePickerResult(int year, int month, int day) {
+        String msg = Integer.toString(month) + "/" + Integer.toString(day) + "/" +
+                Integer.toString(year) + ".";
+        Toast.makeText(this, "Date: " + msg, Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/com/jorgegayer/weight_control/ProfilePage.java
+++ app/src/main/java/com/jorgegayer/weight_control/ProfilePage.java
@@ -3,6 +3,7 @@ package com.jorgegayer.weight_control;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -12,6 +13,7 @@ public class ProfilePage extends AppCompatActivity {
     private EditText editTextWeight;
     private EditText editTextHeight;
     private EditText editTextGoal;
+    private Button saveProfile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,23 +24,62 @@ public class ProfilePage extends AppCompatActivity {
         editTextWeight = findViewById(R.id.editTextWeight);
         editTextHeight = findViewById(R.id.editTextHeight);
         editTextGoal = findViewById(R.id.editTextGoal);
+        saveProfile = findViewById(R.id.btnSaveProfile);
+
+        if (savedInstanceState !=null) {
+            editTextName.setText(savedInstanceState.getString("editTextName"));
+            editTextWeight.setText(savedInstanceState.getString("editTextWeight"));
+            editTextHeight.setText(savedInstanceState.getString("editTextHeight"));
+            editTextGoal.setText(savedInstanceState.getString("editTextGoal"));
+        }
+
     }
-
-    public void saveProfile(View view) {
-        String name;
-        float weight;
-        float height;
-        float goal;
-
+    private boolean validateFields() {
         if(editTextName.length() == 0 || editTextWeight.length() == 0 || editTextHeight.length() == 0 || editTextGoal.length() == 0) {
-            Toast.makeText(this, "Please complete all fields", Toast.LENGTH_LONG).show();
+            Toast.makeText(this, R.string.completeFields, Toast.LENGTH_LONG).show();
+            return false;
         }
-        else {
-            name = editTextName.getText().toString();
-            weight = Float.parseFloat(editTextWeight.getText().toString());
-            height = Float.parseFloat(editTextHeight.getText().toString());
-            goal = Float.parseFloat(editTextGoal.getText().toString());
-            //Save the information into the database
+        if ( Float.parseFloat(editTextWeight.getText().toString()) < 20 || Float.parseFloat(editTextWeight.getText().toString()) >1000) {
+            Toast.makeText(this, R.string.validateWeight, Toast.LENGTH_LONG).show();
+            return false;
         }
+
+        if ( Float.parseFloat(editTextGoal.getText().toString()) < 20 || Float.parseFloat(editTextGoal.getText().toString()) >1000) {
+            Toast.makeText(this, R.string.validateTextGoal, Toast.LENGTH_LONG).show();
+            return false;
+        }
+        if ( Float.parseFloat(editTextHeight.getText().toString()) <= 20 || Float.parseFloat(editTextHeight.getText().toString()) >300) {
+            Toast.makeText(this, R.string.validateHeight, Toast.LENGTH_LONG).show();
+            return false;
+        }
+        return true;
     }
-}
+    public void saveProfile(View view) {
+            String name;
+            float weight;
+            float height;
+            float goal;
+
+            if (validateFields()) {
+                name = editTextName.getText().toString();
+                weight = Float.parseFloat(editTextWeight.getText().toString());
+                height = Float.parseFloat(editTextHeight.getText().toString());
+                goal = Float.parseFloat(editTextGoal.getText().toString());
+                //Save the information into the database
+                Toast.makeText(this, R.string.profileUpdated, Toast.LENGTH_LONG).show();
+                finish();
+            }
+        }
+        @Override
+        public void onSaveInstanceState(Bundle outState) {
+            super.onSaveInstanceState(outState);
+            outState.putString("editTextName", editTextName.getText().toString());
+            outState.putString("editTextHeight", editTextHeight.getText().toString());
+            outState.putString("editTextWeight", editTextWeight.getText().toString());
+            outState.putString("editTextGoal", editTextGoal.getText().toString());
+        }
+
+
+    }
+
+
